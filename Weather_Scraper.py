cda6776cc1d6869fe178a7246cf9f1e62816f68f
Jason-Hlavac <49419324+Jason-Hlavac@users.noreply.github.com>
2019-04-09 22:04:56 -0700

    Update Weather_Scraper.py

--- Weather_Scraper.py
+++ Weather_Scraper.py
@@ -4,10 +4,13 @@ import requests
 page = requests.get("https://weather.com/weather/today/l/USCA1209:1:US")
 soup = BeautifulSoup(page.content, "html.parser")
 temperature= soup.find("div", class_="today_nowcard-temp").get_text()
-
+time = ()
 page = requests.get("https://weather.com/weather/tenday/l/USCA1209:1:US")
 soup = BeautifulSoup(page.content, "html.parser")
 city = soup.find("div", class_="locations-title ten-day-page-title").get_text()
-days = soup.find("span", class_="date-time").get_text()
-city = city.replace("10 Day Weather5:32 pm PDTPrint", " ", 1)
-print("Weather forecast for {} \nCurrent Temperature: {}\n{}".format(city, temperature, days))
+tenday = soup.find("table", class_="twc-table").get_text()
+if (city[-16] == 1):
+    city = city[0:-32]
+else:
+    city = city[0:-31]
+print("Weather forecast for {} \nCurrent Temperature: {}\n{}".format(city, temperature, tenday))
